e3ec852ead13b1289d83e4484e95d980398df7c5
YetAnotherNotHacking <yetanotherdingus@gmail.com>
2026-05-28 07:56:38 -0600

    feeder pulsing logic for flywheel momentum preservatoin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/ShooterConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/ShooterConstants.java
@@ -6,8 +6,7 @@ import com.bylazar.configurables.annotations.Configurable;
 public class ShooterConstants {
     public static double TICKS_PER_REV = 28.0;
 
-    // it should be noted that we have switched to bang bang,
-    // as it updates faster than this PID.
+    // back in use
     public static double PID_P = 120.0;
     public static double PID_I = 0.006;
     public static double PID_D = 0.0;
@@ -15,12 +14,16 @@ public class ShooterConstants {
 
     public static double DEFAULT_TARGET_RPM = 4300.0;
     public static double MAX_TARGET_RPM = 6000.0;
-    public static double CYCLE_A_TARGET_RPM = 4300.0;
-    public static double CYCLE_B_TARGET_RPM = 4300.0;
+    public static double CYCLE_A_TARGET_RPM = 4490.0;
+    public static double CYCLE_B_TARGET_RPM = 4190.0;
 
-    public static double FEEDER_MANUAL_POWER = 1.00;
-    public static double FEEDER_INDEX_POWER = 0.5;
-    public static double FEEDER_ALPHA_STOP_THRESHOLD = 450.0;
+    public static double FEEDER_MANUAL_POWER = 0.90;
+    public static double FEEDER_INDEX_POWER = 0.30;
+    public static double FEEDER_ALPHA_STOP_THRESHOLD = 380.0;
+
+    public static long FEEDER_PULSE_RUN_TIME_MS = 350;
+    public static long CYCLE_A_FEEDER_PULSE_PAUSE_TIME_MS = 1000; // far
+    public static long CYCLE_B_FEEDER_PULSE_PAUSE_TIME_MS = 450; // close
 
     public static double SHOOTER_TRIGGER_DEADBAND = 0.02;
 
